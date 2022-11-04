a8c3bcc486b5070a6ff2a10c0458b7868b439a10
acmeplus <ctalon@gmail.com>
2022-11-04 17:21:52 +0000

    Egret Retroarch autorotation and paddle support
    * Added retroarch patch to support dynamic screen orientation change
    * Added initial Egret-II Trackball and Paddle controller configuration

--- board/batocera/allwinner/r16/acm/fsoverlay/usr/bin/rotate_screen.sh
+++ board/batocera/allwinner/r16/acm/fsoverlay/usr/bin/rotate_screen.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 ROTATE=$1
+HDMISTATUS=$(cat /sys/devices/virtual/switch/hdmi/state)
 
-if [[ ${ROTATE} == "1" ]]; then
+if [[ ${ROTATE} == "1" ]] || [[ ${HDMISTATUS} == "1" ]]; then
 	batocera-settings-set display.rotate 0
-        batocera-settings-set global.retroarch.aspect_ratio_index 8
         curl http://localhost:1234/quit
 else
 	batocera-settings-set display.rotate 1
-        batocera-settings-set global.retroarch.aspect_ratio_index 0
         curl http://localhost:1234/quit
 fi
 
--- package/batocera/controllers/e2m-paddle-trackball/e2m-paddle-trackball.mk
+++ package/batocera/controllers/e2m-paddle-trackball/e2m-paddle-trackball.mk
@@ -1,15 +1,15 @@
 ################################################################################
 #
-# aelightgun
+# Egret-II Mini Trackball and Paddle accessory
 #
 ################################################################################
-AELIGHTGUN_VERSION = 1
-AELIGHTGUN_LICENSE = GPL
-AELIGHTGUN_SOURCE=
+E2M_PADDLE_TRACKBALL_VERSION = 1
+E2M_PADDLE_TRACKBALL_LICENSE = GPL
+E2M_PADDLE_TRACKBALL_SOURCE=
 
-define AELIGHTGUN_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/aelightgun/99-aelightgun.rules $(TARGET_DIR)/etc/udev/rules.d/99-aelightgun.rules
-	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/aelightgun/virtual-aelightgun-add $(TARGET_DIR)/usr/bin/virtual-aelightgun-add
+define E2M_PADDLE_TRACKBALL_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/e2m-paddle-trackball/99-e2m-paddle-trackball.rules $(TARGET_DIR)/etc/udev/rules.d/99-e2m-paddle-trackball.rules
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/e2m-paddle-trackball/virtual-e2m-paddle-trackball-add $(TARGET_DIR)/usr/bin/virtual-e2m-paddle-trackball-add
 endef
 
 $(eval $(generic-package))
